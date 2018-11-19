94694c2d07d25015c8e770601b41266c72dc11d3
daposy <endqwerty@gmail.com>
2018-11-18 17:39:45 -0800

    add unit test

--- tests/unit/organizationUnitTests.js
+++ tests/unit/organizationUnitTests.js
@@ -1,17 +1,7 @@
-/*
-var assert = require('assert');
+var assert = require("assert");
 
-suite('Organizations', function() {
-	test('can be edited by an admin', function(done, server) {
-		server.eval(function() {
-			organizationId = Organizations.find({}) 
-			adminUser = 
-			emit('adminCanEdit', Organizations.canEdit(, organizationId));
-		});
-
-		server.once('adminCanEdit', function(canEdit) {
-			assert.equal(canEdit, true);
-		});
-	});
+suite("Demo suite", function() {
+  test("demo", function() {
+    assert.equal(true, true);
+  });
 });
-*/
