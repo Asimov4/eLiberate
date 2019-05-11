d762df5eb10fa0c765a893f5373ab5011ffb265c
Asimov4 <asimov4@gmail.com>
2019-05-11 19:16:20 +0000

    Add tool tip to explain what is required to get quorum

--- Client/messages.js
+++ Client/messages.js
@@ -1,43 +1,49 @@
-if(Meteor.isClient) {
-	Template.messageTemplates.helpers ({
-		meeting: function() {
-			return Meetings.findOne({_id: Session.get("meetingId")});
+if (Meteor.isClient) {
+	Template.messageTemplates.helpers({
+		meeting: function () {
+			return Meetings.findOne({ _id: Session.get("meetingId") });
 		},
 
-		messages: function() {
-			return Messages.find({ meetingId: Session.get("meetingId")});
+		messages: function () {
+			return Messages.find({ meetingId: Session.get("meetingId") });
 		},
 
-		rulesetHeader: function() {
+		rulesetHeader: function () {
 			return toCamelCase(Session.get("ruleset")) + "Header";
 		},
 
-		rulesetTools: function() {
+		rulesetTools: function () {
 			return toCamelCase(Session.get("ruleset")) + "Tools";
 		},
 
-		rulesetMessages: function() {
+		rulesetMessages: function () {
 			return toCamelCase(Session.get("ruleset")) + "Messages";
 		},
 
-		rulesetControls: function() {
+		rulesetControls: function () {
 			return toCamelCase(Session.get("ruleset")) + "Controls";
 		},
 
-		attendance: function() {
-			return Attendees.find({meetingId: Session.get("meetingId")}).count();
+		attendance: function () {
+			return Attendees.find({ meetingId: Session.get("meetingId") }).count();
 		},
 
-		quorum: function() {
-			var meeting = Meetings.findOne({_id: Session.get("meetingId")});
-			var attendanceCount = Attendees.find({meetingId: meeting._id}).count();
-			var members = Permissions.find({organizationId: meeting.organizationId}).count();
+		members: function () {
+			var meeting = Meetings.findOne({ _id: Session.get("meetingId") });
+			return Permissions.find({ organizationId: meeting.organizationId, $or: [{ role: ROLES.administrator }, { role: ROLES.chairperson }, { role: ROLES.member }] }).count();
+		},
 
-			if(attendanceCount / members >= .5) {
+		quorum: function () {
+			var meeting = Meetings.findOne({ _id: Session.get("meetingId") });
+			var attendanceCount = Attendees.find({ meetingId: meeting._id }).count();
+			var members = Permissions.find({ organizationId: meeting.organizationId, $or: [{ role: ROLES.administrator }, { role: ROLES.chairperson }, { role: ROLES.member }] }).count();
+
+			if (attendanceCount / members >= .5) {
 				return "Quorum";
-			} else {
+			}
+			else {
 				return "No Quorum";
 			}
 		}
 	});
- }
+}
